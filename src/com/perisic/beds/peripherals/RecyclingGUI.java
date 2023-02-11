b502b55c5d8c99be1caa491205c824e3a3c07855
Yashitha Nadiranga <yasitha.online100@gmail.com>
2023-02-11 21:51:04 +0530

    First Commit

---
+++ src/com/perisic/beds/peripherals/RecyclingGUI.java
@@ -0,0 +1,115 @@
+package com.perisic.beds.peripherals;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import com.perisic.beds.recycling.CustomerPanel;
+import com.perisic.beds.recycling.ReceiptPrinter;
+/**
+ * A Simple Graphical User Interface for the Recycling Machine.
+ * This class combines three different aspects: 
+ * 1) Starting the software
+ * 2) Implementing the GUI
+ * 3) Handling events
+ * 4) Handling output
+ *  
+ * @author Marc Conrad
+ *
+ */
+public class RecyclingGUI extends JFrame implements ActionListener, ReceiptPrinter  {
+
+	private static final long serialVersionUID = -1077856539035586635L;
+	
+	/**
+	 * Method that is called when an item has been entered into the machine or a receipt has been requested.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if( e.getSource().equals(slot1)) { 
+			theRecyclingMachine.itemReceived(1);
+		} else if( e.getSource().equals(slot2)) { 
+			theRecyclingMachine.itemReceived(2);
+		} else if(e.getSource().equals(slot3)) { 
+			theRecyclingMachine.itemReceived(3);
+		} else if(e.getSource().equals(slot4)) { 
+			theRecyclingMachine.itemReceived(4);
+		}else if(e.getSource().equals(receipt)) { 
+			theRecyclingMachine.printReceipt();
+		}
+	}
+
+	JButton slot1 = new JButton("Can"); 
+	JButton slot2 = new JButton("Bottle"); 
+	JButton slot3 = new JButton("Crate"); 
+	JButton slot4 = new JButton("Test"); 
+	
+
+	JButton receipt = new JButton("Receipt"); 
+	JTextArea outputWindow = new JTextArea(10,25);
+	
+	CustomerPanel theRecyclingMachine = null;
+/**
+ * Constructor; sets up GUI.
+ */
+	public RecyclingGUI() {
+		super();
+		setSize(350,350);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		JPanel panel = new JPanel(); 
+		panel.setBackground(Color.BLUE);
+		panel.add(slot1); 
+		panel.add(slot2);
+		panel.add(slot3); 
+		panel.add(slot4); 
+
+		slot1.addActionListener(this); 
+		slot2.addActionListener(this); 
+		slot3.addActionListener(this); 
+		slot4.addActionListener(this); 
+		
+		slot1.setBackground(Color.white);
+		slot2.setBackground(Color.white);
+		slot3.setBackground(Color.white);
+		slot4.setBackground(Color.white);
+		receipt.setBackground(Color.white);
+
+		panel.add(receipt); 
+		receipt.addActionListener(this); 
+
+		JScrollPane scrollPane = new JScrollPane(outputWindow); 		
+		outputWindow.setEditable(false);
+		
+		outputWindow.setText("Welcome to the Recycling Machine!");
+		outputWindow.setSize(630, 600);
+		panel.add(scrollPane);
+		
+
+		getContentPane().add(panel);
+		panel.repaint();
+		
+		theRecyclingMachine = new CustomerPanel(this); 
+	}
+	/**
+	 * Main entry point into the recycling machine.
+	 * @param args not used.
+	 */
+	public static void main(String [] args ) { 
+		RecyclingGUI myGUI = new RecyclingGUI(); 
+		myGUI.setVisible(true); 
+
+	}
+	
+	/**
+	 * Prints text in the output window.
+	 */
+	@Override
+	public void print(String str) { 
+			outputWindow.setText(str); 
+			outputWindow.repaint();
+		
+		
+	}
+
+}
---
+++ src/com/perisic/beds/recycling/Test.java
@@ -0,0 +1,9 @@
+package com.perisic.beds.recycling;
+
+public class Test extends DepositItem {
+	
+	public Test() {
+		value = 22;
+	}
+
+}
